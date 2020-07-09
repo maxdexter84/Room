52e35dba5bc232e2abc82df9cfe2e8bd9a0d41f7
maxdexter <maxdexter@bk.ru>
2020-07-09 17:16:39 +0300

    добавил анотации room и геттеры с сеттерами в класс Song

--- app/src/main/java/com/maxdexter/room/model/Song.java
+++ app/src/main/java/com/maxdexter/room/model/Song.java
@@ -9,6 +9,35 @@ public class Song {
     @PrimaryKey //Первичный ключ, идентификатор статьи
     @ColumnInfo(name = "id") //обозночаем столбец, задаем имя
     private String mId;
+    @ColumnInfo(name = "name")
     private String mName;
+    @ColumnInfo(name = "duration")
     private String mDuration;
+
+    
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getDuration() {
+        return mDuration;
+    }
+
+    public void setDuration(String duration) {
+        mDuration = duration;
+    }
+
+
 }
