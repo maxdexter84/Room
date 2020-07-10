7c28287b9e963cc197fccd32e876b5cf00dae63a
maxdexter <maxdexter@bk.ru>
2020-07-10 10:05:01 +0300

    создал класс Song

--- app/src/main/java/com/maxdexter/room/database/Album.java
+++ app/src/main/java/com/maxdexter/room/database/Album.java
@@ -1,4 +1,46 @@
 package com.maxdexter.room.database;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Album {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int mId;
+    @ColumnInfo(name = "name")
+    private String mName;
+    @ColumnInfo(name = "release")
+    private String mReleaseDate;
+
+    public Album(int id, String name, String releaseDate) {
+        mId = id;
+        mName = name;
+        mReleaseDate = releaseDate;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getReleaseDate() {
+        return mReleaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        mReleaseDate = releaseDate;
+    }
 }
